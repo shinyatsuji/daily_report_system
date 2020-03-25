88c194e9cabc2976be096871a54e891e9a8928d8
shinyatsuji <mb46nh82ml@icloud.com>
2020-03-25 17:09:42 +0900

    Add login/logout link

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,7 +1,5 @@
 package controllers.toppage;
 
-
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -31,6 +29,10 @@ public class TopPageIndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if (request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
